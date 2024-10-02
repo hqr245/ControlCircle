30c4e1bbc87903bbbc35234cb3f455c24a0e877d
Antonio Caro <antoniojosecaropozo@gmail.com>
2024-10-02 13:09:37 +0200

    FIXED: fixed the position of the buttons, the design of the circle (now it is a circumference) and the way the code adds the event handler (now is a named class.)

--- src/main/java/es/ieslosmontecillos/controlcircle/ControlCircle.java
+++ src/main/java/es/ieslosmontecillos/controlcircle/ControlCircle.java
@@ -1,14 +1,16 @@
 package es.ieslosmontecillos.controlcircle;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
@@ -20,13 +22,11 @@ public class ControlCircle extends Application {
 
         //Setting up the Scene
         Group root = new Group();
-        Scene scene = new Scene(root, 320, 240);
+        Scene scene = new Scene(root, 330, 240);
         stage.setTitle("ControlCircle");
         stage.setScene(scene);
-
         //Adding the pane where our components belongs
         FlowPane flowPane = new FlowPane();
-
 
         //creating a container
 
@@ -36,20 +36,53 @@ public class ControlCircle extends Application {
 
         Circle circle = new Circle();
         circle.setRadius(100);
+        circle.setFill(scene.getFill());
+        circle.setStroke(Color.BLACK);
         //creating both the buttons
-        Button increase = new Button("Increase");
-        increase.setOnAction(e -> {circle.setRadius(circle.getRadius() + 10);});
+        Button btnIncrease = new Button("Increase");
+
+        EventHandler<ActionEvent> increaseEv = new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                circle.setRadius(circle.getRadius() + 10);
+                stage.setHeight(stage.getHeight() +20);
+                stage.setWidth(stage.getWidth() +20);
+            }
+        };
+        btnIncrease.setOnAction(increaseEv);
+
+        circle.radiusProperty().addListener((observable, oldValue, newValue) -> {
+
+            if(newValue.intValue()<0){
+                circle.setRadius(0);
+
+                stage.setHeight(stage.getHeight() +20);
+                stage.setWidth(stage.getWidth() +20);
+            }
+
+        });
 
         Button decrease = new Button("Decrease");
-        decrease.setOnAction(e -> {circle.setRadius(circle.getRadius() - 10);});
+        EventHandler<ActionEvent> decreaseEv = new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                circle.setRadius(circle.getRadius() - 10);
+                stage.setHeight(stage.getHeight() - 20);
+                stage.setWidth(stage.getWidth() - 20);
+
+
+            }
+
+        };
+        decrease.setOnAction(decreaseEv);
 
 
 
-        hbox.getChildren().add(increase);
+        hbox.getChildren().add(btnIncrease);
         hbox.getChildren().add(decrease);
 
-        vbox.getChildren().add(hbox);
         vbox.getChildren().add(circle);
+        vbox.getChildren().add(hbox);
 
 
         flowPane.getChildren().add(vbox);
